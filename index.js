39655feea2a9190ba2553af3521bf65c9771d0b4
heber497 <heberantonutti@gmail.com>
2022-09-29 20:41:07 -0300

    libreria

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ class Producto {
     this.imagen = imagen;
   }
 }
-
 class Carrito {
   constructor(id) {
     this.id = id;
     this.productos = [];
   }
-
   calcularTotal() {
     let total = 0;
     for (let i = 0; i < this.productos.length; i++) {
@@ -21,28 +19,20 @@ class Carrito {
     return total;
   }
 }
-
 /* FUNCIONES */
-
-function main() {
-  confirmarCompra();
-  btnVaciarCarro();
-}
-
 function individualTarjeta(producto) {
-  let tarjetaIndividual = ` 
+  let tarjetaIndividual = `
                 <div class="card" >
                 <img src="../public/img/${producto.imagen}" alt="">
                 <h5>${producto.nombre}</h5>
                 <p>$ ${producto.precio}</p>
-                <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carro</a>
+                <a class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carro</a>
             </div>`;
-
   return tarjetaIndividual;
 }
-
 function tarjetaCarro(producto) {
   let carroTarjeta = `
+
                         <hr><div class="tarjeta-carrito">
                             <div class="imagen">
                                 <img src="../public/img/${producto.imagen}" alt="">
@@ -56,7 +46,13 @@ function tarjetaCarro(producto) {
                         </div><hr>`;
   return carroTarjeta;
 }
-
+function btnLimpiarCarro() {
+  let botonLimpiarCarrito = `
+                            <div class="container limpiarCarro">
+                                <a   id="vaciarCarrito" class="btn btn-danger">Vaciar Carrito</a>
+                            </div>`;
+  return botonLimpiarCarrito;
+}
 function limpiarCarrito() {
   let divCarrito = document.querySelector("#carrito");
   divCarrito.innerHTML = "";
@@ -68,19 +64,74 @@ function actualizarCarrito(carrito) {
     divCarrito.innerHTML += tarjetaCarro(producto);
   });
   divCarrito.innerHTML += `<h1>Total: $ ${carrito.calcularTotal()}</h1>`;
+  divCarrito.innerHTML += `
+                            <select name="cuotas" id="cuotas">
+                                <option value="1 cuota">1 cuota sin interes de $${carrito.calcularTotal()}</option>
+                                <option value="3 cuotas">3 cuotas sin interes de $${
+                                  carrito.calcularTotal() / 3
+                                }</option>
+                                <option value="6 cuotas">6 cuotas sin interes de $${
+                                  carrito.calcularTotal() / 6
+                                }</option>
+                                <option value="12 cuotas">12 cuotas fijas de $${
+                                  (carrito.calcularTotal() * 1.3) / 12
+                                }</option>
+                            </select>`;
+  divCarrito.innerHTML += `
+                            <div class="container comprar">
+                                <a id="confirmarCompra" class="btn btn-success">Comprar</a>
+                            </div>`;
 }
 
 function renovarStorage() {
   localStorage.removeItem("carrito");
   localStorage.setItem("carrito", JSON.stringify(carrito));
 }
+function vaciarCarrito() {
+  let divCarrito = document.querySelector("#carrito");
+  divCarrito.innerHTML = "";
+  carrito = new Carrito();
+}
 
-// function obtenerProductosLocalStorage() {
-//   let productosAlmacenados = localStorage.getItem("productos");
-//   if (productosAlmacenados !== null) {
-//     Producto = JSON.parse(productosAlmacenados);
-//   }
-// }
+function alert() {
+  Swal.fire({
+    icon: "success",
+    title: "¡Gracias por tu compra!",
+    text: "Pronto recibiras toda la info en tu mail",
+  });
+}
+
+function alertError() {
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "No has seleccionado ningun producto",
+  });
+}
+
+function toast(producto) {
+  Toastify({
+    text: `Agregaste ${producto.nombre} al carrito`,
+    duration: 2000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      color: "black",
+      background: "linear-gradient(to right, #dddddd, #d2d2d2)",
+    },
+  }).showToast();
+}
+
+function confirmarCompra() {
+  let botonConfirmarCompra = document.getElementById("confirmarCompra");
+  botonConfirmarCompra.addEventListener("click", () => {
+    alert();
+    vaciarCarrito();
+    renovarStorage();
+  });
+}
 
 /* CARGAR CARRITO EXISTENTE */
 
@@ -93,11 +144,8 @@ window.addEventListener("DOMContentLoaded", (e) => {
   limpiarCarrito();
   actualizarCarrito(carritoGuardado);
 });
-
 /* GENERACION CATALOGO DE PRODUCTOS*/
-
 let Catalogo = [];
-
 let producto1 = new Producto(1, "M.Estudiante", 350, "Bangho.png");
 let producto2 = new Producto(2, "M.Diseño", 650, "Dell.png");
 let producto3 = new Producto(3, "M.Gamer", 1000, "asus tuf.png");
@@ -107,9 +155,7 @@ Catalogo.push(producto1);
 Catalogo.push(producto2);
 Catalogo.push(producto3);
 Catalogo.push(producto4);
-
 /* GENERACION DE TARJETAS DE PRODUCTOS*/
-
 let tarjetasDiv = document.querySelector("#tarjetas");
 Catalogo.forEach((producto) => {
   tarjetasDiv.innerHTML += individualTarjeta(producto);
@@ -117,7 +163,7 @@ Catalogo.forEach((producto) => {
 
 /* carrito De un producto */
 
-let carrito = new Carrito(1);
+let carrito = new Carrito();
 let botones = document.querySelectorAll(".botonDeCompra");
 let arrayDeBotones = Array.from(botones);
 arrayDeBotones.forEach((boton) => {
@@ -126,299 +172,17 @@ arrayDeBotones.forEach((boton) => {
       (producto) => producto.id == e.target.id
     );
     carrito.productos.push(productoSeleccionado);
+    toast(productoSeleccionado);
     limpiarCarrito();
     actualizarCarrito(carrito);
     renovarStorage();
+    confirmarCompra();
   });
 });
 
-// class Producto {
-//   constructor(id, nombre, precio, imagen) {
-//     this.id = id;
-//     this.nombre = nombre;
-//     this.precio = precio;
-//     this.imagen = imagen;
-//   }
-// }
+let botonVaciarCarrito = document.querySelector("#vaciarCarrito");
 
-// class Carrito {
-//   constructor(id) {
-//     this.id = id;
-//     this.productos = [];
-//   }
-
-//   calcularTotal() {
-//     let total = 0;
-//     for (let i = 0; i < this.productos.length; i++) {
-//       total += this.productos[i].precio;
-//     }
-//     return total;
-//   }
-// }
-
-// /* FUNCIONES */
-
-// function individualTarjeta(producto) {
-//   let tarjetaIndividual = `
-//                 <div class="card" >
-//                 <img src="../public/img/${producto.imagen}" alt="">
-//                 <h5>${producto.nombre}</h5>
-//                 <p>$ ${producto.precio}</p>
-//                 <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carro</a>
-//             </div>`;
-
-//   return tarjetaIndividual;
-// }
-
-// function tarjetaCarro(producto) {
-//   let carroTarjeta = `
-//                         <hr><div class="tarjeta-carrito">
-//                             <div class="imagen">
-//                                 <img src="../public/img/${producto.imagen}" alt="">
-//                             </div>
-//                             <div >
-//                                 <h5>${producto.nombre}</h5>
-//                             </div>
-//                             <div>
-//                                 <h5>$ ${producto.precio}</h5>
-//                             </div>
-//                         </div><hr>`;
-//   return carroTarjeta;
-// }
-
-// function limpiarCarrito() {
-//   let divCarrito = document.querySelector("carrito");
-//   divCarrito.innerHTML = "";
-// }
-
-// function actualizarCarrito(carrito) {
-//   let divCarrito = document.querySelector("#carrito");
-//   carrito.productos.forEach((producto) => {
-//     divCarrito.innerHTML += tarjetaCarro(producto);
-//   });
-//   divCarrito.innerHTML += `<h1>Total: $ ${carrito.calcularTotal()}</h1>`;
-// }
-
-// function renovarStorage() {
-//   localStorage.removeItem("carrito");
-//   localStorage.setItem("carrito", JSON.stringify(carrito));
-// }
-
-// // function obtenerProductosLocalStorage() {
-// //   let productosAlmacenados = localStorage.getItem("productos");
-// //   if (productosAlmacenados !== null) {
-// //     Producto = JSON.parse(productosAlmacenados);
-// //   }
-// // }
-
-// /* CARGAR CARRITO EXISTENTE */
-
-// window.addEventListener("DOMContentLoaded", (e) => {
-//   let storage = JSON.parse(localStorage.getItem("carrito"));
-//   let carritoGuardado = new Carrito(storage.id, storage.productos);
-//   storage.productos.forEach((producto) => {
-//     carritoGuardado.productos.push(producto);
-//   });
-//   limpiarCarrito();
-//   actualizarCarrito(carritoGuardado);
-// });
-
-// /* GENERACION CATALOGO DE PRODUCTOS*/
-
-// let Catalogo = [];
-
-// let producto1 = new Producto(1, "M.Estudiante", 350, "Bangho.png");
-// let producto2 = new Producto(2, "M.Diseño", 650, "Dell.png");
-// let producto3 = new Producto(3, "M.Gamer", 1000, "asus tuf.png");
-// let producto4 = new Producto(4, "M.Pro-Gamer", 2000, "asus pro.png");
-
-// Catalogo.push(producto1);
-// Catalogo.push(producto2);
-// Catalogo.push(producto3);
-// Catalogo.push(producto4);
-
-// /* GENERACION DE TARJETAS DE PRODUCTOS*/
-
-// let tarjetasDiv = document.querySelector("#tarjetas");
-// Catalogo.forEach((producto) => {
-//   tarjetasDiv.innerHTML += individualTarjeta(producto);
-// });
-
-// /* carrito De un producto */
-
-// let carrito = new Carrito(1);
-// let botones = document.querySelectorAll(".botonDeCompra");
-// let arrayDeBotones = Array.from(botones);
-// arrayDeBotones.forEach((boton) => {
-//   boton.addEventListener("click", (e) => {
-//     let productoSeleccionado = Catalogo.find(
-//       (producto) => producto.id == e.target.id
-//     );
-
-//     carrito.productos.push(productoSeleccionado);
-//     limpiarCarrito();
-//     actualizarCarrito(carrito);
-//     renovarStorage();
-//   });
-// });
-
-//
-//
-//
-//
-// function inicio() {
-//   alert("Bienvenidos a nwwPC. Importamos Netbook al mejor precio!");
-//   let usuario = prompt("Ingresa tu nombre:");
-//   alert(
-//     "Hola " +
-//       usuario +
-//       " " +
-//       "Mira la lista de los nuevos ingresos de este mes."
-//   );
-// }
-
-// function productos() {
-//   let lista;
-//   do {
-//     lista = parseInt(
-//       prompt(
-//         "\n1 : Netbook modelo estudio marca Bangho con un valor de: $350. \n2 : Netbook modelos diseño marca Dell con un valor de: $600. \n3 : Netbook modelo gamer marca Asus con un valor de: $1000. "
-//       )
-//     );
-//   } while (lista != 1 && lista != 2 && lista != 3);
-
-//   switch (lista) {
-//     case 1:
-//       return "Modelo Estudiante";
-//     case 2:
-//       return "Modelo Diseño";
-//     case 3:
-//       return "Modelo Gamer";
-//   }
-// }
-
-// function costo(lista) {
-//   if (lista === "Modelo Estudiante") {
-//     return 350;
-//   } else if (lista === "Modelo Diseño") {
-//     return 600;
-//   } else if (lista === "Modelo Gamer") {
-//     return 1000;
-//   } else {
-//     alert("no existe esa opcion");
-//   }
-// }
-
-// function pagar(cantidad, costo) {
-//   alert("Usted seleccionó lo siguiente: " + cantidad + "\nCosto: $" + costo);
-//   let dinero = parseInt(prompt("¿Con cuanto pagara?"));
-//   if (dinero >= costo) {
-//     alert(
-//       "exelente , su vuelto es " +
-//         "$" +
-//         (dinero - costo) +
-//         "," +
-//         " solo queda cordinar la entrega. Muchas gracias."
-//     );
-//   } else {
-//     alert("lo siento. es poco dinero");
-//   }
-// }
-
-// inicio();
-// let cantidad = productos();
-// let valor = costo(cantidad);
-// pagar(cantidad, valor);
-
-// const verMasEstudiante = document.querySelector("#verMasEstudiante");
-// const textoEstudiante = document.querySelector("p");
-
-// verMasEstudiante.addEventListener("click", () => {
-//   if (textoEstudiante.classList.contains("caracteristicas")) {
-//     textoEstudiante.classList.remove("caracteristicas");
-//   } else {
-//     textoEstudiante.classList.add("caracteristicas");
-//   }
-// });
-
-// const verMasDisenio = document.querySelector("#verMasDisenio");
-// const textoDisenio = document.getElementById("textoDisenio");
-
-// verMasDisenio.addEventListener("click", () => {
-//   if (textoDisenio.classList.contains("caracteristicas")) {
-//     textoDisenio.classList.remove("caracteristicas");
-//   } else {
-//     textoDisenio.classList.add("caracteristicas");
-//   }
-// });
-
-// const verMasGamer = document.querySelector("#verMasGamer");
-// const textoGamer = document.getElementById("textoGamer");
-
-// verMasGamer.addEventListener("click", () => {
-//   if (textoGamer.classList.contains("caracteristicas")) {
-//     textoGamer.classList.remove("caracteristicas");
-//   } else {
-//     textoGamer.classList.add("caracteristicas");
-//   }
-// });
-
-// console.log(verMasGamer);
-
-// verMasGamer.addEventListener("click", () => {});
-
-// let final = document.getElementById("producto");
-// final.innerHTML =
-//   "<h2> gracias por tu compra espero que difrutes tu netbook modelo:<h2>" +
-//   " " +
-//   cantidad;
-
-// document.body.appendChild(final);
-
-// function filtraNetbook(netbooks, filtro) {
-//   const filtrado = netbooks.filter((el) => {
-//     return el.marca.includes(filtro);
-//   });
-//   return filtrado;
-// }
-// let ingreso = prompt(
-//   "Antes de empesar? tenes experiensa en hadware pc? Queres sabes mas detalle de los equipos que tenemos 💻😋? Escribe la marca de los productos publicados y lo veras reflejado por consola 😎"
-// );
-
-// const resultado = filtraNetbook(netbooks, ingreso);
-
-// console.log(resultado);
-
-// const netbooks = [
-//   {
-//     id: 1,
-//     marca: "bangho",
-//     modelo: "estudiante",
-//     caracteristica:
-//       "Max L4 i1 gris oscura 14" +
-//       "," +
-//       "Intel Celeron N4000 4GB de RAM 120GB SSD, Intel UHD Graphics 600 1366x768px Windows 10 Home",
-//   },
-//   {
-//     id: 2,
-//     marca: "dell",
-//     modelo: "diseño",
-//     caracteristica: "Inspiron 15 4580 I5 Win11 256ssd 8g Ram gtx 1650",
-//   },
-//   {
-//     id: 3,
-//     marca: "asus",
-//     modelo: "gamer",
-//     caracteristica: "Tuf Core I7 16gb Ssd 512gb Rtx 3060",
-//   },
-// ];
-// function inicio() {
-//   alert("Bienvenidos a nwwPC. Importamos Netbook al mejor precio!");
-//   let usuario = prompt("Ingresa tu nombre:");
-//   alert(
-//     "Hola " +
-//       usuario +
-//       " " +
-//       "Mira la lista de los nuevos ingresos de este mes."
-//   );
-// }
+botonVaciarCarrito.addEventListener("click", () => {
+  vaciarCarrito();
+  renovarStorage();
+});
