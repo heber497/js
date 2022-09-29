92e92781b175c738d68933af90c448b4c19b6cfe
heber497 <heberantonutti@gmail.com>
2022-09-29 17:28:04 -0300

    storage

--- index.js
+++ index.js
@@ -24,10 +24,15 @@ class Carrito {
 
 /* FUNCIONES */
 
+function main() {
+  confirmarCompra();
+  btnVaciarCarro();
+}
+
 function individualTarjeta(producto) {
   let tarjetaIndividual = ` 
                 <div class="card" >
-                <img src="/public/img/${producto.imagen}" alt="">
+                <img src="../public/img/${producto.imagen}" alt="">
                 <h5>${producto.nombre}</h5>
                 <p>$ ${producto.precio}</p>
                 <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carro</a>
@@ -40,7 +45,7 @@ function tarjetaCarro(producto) {
   let carroTarjeta = `
                         <hr><div class="tarjeta-carrito">
                             <div class="imagen">
-                                <img src= "/public/img/${producto.imagen}" alt="">
+                                <img src="../public/img/${producto.imagen}" alt="">
                             </div>
                             <div >
                                 <h5>${producto.nombre}</h5>
@@ -127,6 +132,140 @@ arrayDeBotones.forEach((boton) => {
   });
 });
 
+// class Producto {
+//   constructor(id, nombre, precio, imagen) {
+//     this.id = id;
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.imagen = imagen;
+//   }
+// }
+
+// class Carrito {
+//   constructor(id) {
+//     this.id = id;
+//     this.productos = [];
+//   }
+
+//   calcularTotal() {
+//     let total = 0;
+//     for (let i = 0; i < this.productos.length; i++) {
+//       total += this.productos[i].precio;
+//     }
+//     return total;
+//   }
+// }
+
+// /* FUNCIONES */
+
+// function individualTarjeta(producto) {
+//   let tarjetaIndividual = `
+//                 <div class="card" >
+//                 <img src="../public/img/${producto.imagen}" alt="">
+//                 <h5>${producto.nombre}</h5>
+//                 <p>$ ${producto.precio}</p>
+//                 <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carro</a>
+//             </div>`;
+
+//   return tarjetaIndividual;
+// }
+
+// function tarjetaCarro(producto) {
+//   let carroTarjeta = `
+//                         <hr><div class="tarjeta-carrito">
+//                             <div class="imagen">
+//                                 <img src="../public/img/${producto.imagen}" alt="">
+//                             </div>
+//                             <div >
+//                                 <h5>${producto.nombre}</h5>
+//                             </div>
+//                             <div>
+//                                 <h5>$ ${producto.precio}</h5>
+//                             </div>
+//                         </div><hr>`;
+//   return carroTarjeta;
+// }
+
+// function limpiarCarrito() {
+//   let divCarrito = document.querySelector("carrito");
+//   divCarrito.innerHTML = "";
+// }
+
+// function actualizarCarrito(carrito) {
+//   let divCarrito = document.querySelector("#carrito");
+//   carrito.productos.forEach((producto) => {
+//     divCarrito.innerHTML += tarjetaCarro(producto);
+//   });
+//   divCarrito.innerHTML += `<h1>Total: $ ${carrito.calcularTotal()}</h1>`;
+// }
+
+// function renovarStorage() {
+//   localStorage.removeItem("carrito");
+//   localStorage.setItem("carrito", JSON.stringify(carrito));
+// }
+
+// // function obtenerProductosLocalStorage() {
+// //   let productosAlmacenados = localStorage.getItem("productos");
+// //   if (productosAlmacenados !== null) {
+// //     Producto = JSON.parse(productosAlmacenados);
+// //   }
+// // }
+
+// /* CARGAR CARRITO EXISTENTE */
+
+// window.addEventListener("DOMContentLoaded", (e) => {
+//   let storage = JSON.parse(localStorage.getItem("carrito"));
+//   let carritoGuardado = new Carrito(storage.id, storage.productos);
+//   storage.productos.forEach((producto) => {
+//     carritoGuardado.productos.push(producto);
+//   });
+//   limpiarCarrito();
+//   actualizarCarrito(carritoGuardado);
+// });
+
+// /* GENERACION CATALOGO DE PRODUCTOS*/
+
+// let Catalogo = [];
+
+// let producto1 = new Producto(1, "M.Estudiante", 350, "Bangho.png");
+// let producto2 = new Producto(2, "M.Diseño", 650, "Dell.png");
+// let producto3 = new Producto(3, "M.Gamer", 1000, "asus tuf.png");
+// let producto4 = new Producto(4, "M.Pro-Gamer", 2000, "asus pro.png");
+
+// Catalogo.push(producto1);
+// Catalogo.push(producto2);
+// Catalogo.push(producto3);
+// Catalogo.push(producto4);
+
+// /* GENERACION DE TARJETAS DE PRODUCTOS*/
+
+// let tarjetasDiv = document.querySelector("#tarjetas");
+// Catalogo.forEach((producto) => {
+//   tarjetasDiv.innerHTML += individualTarjeta(producto);
+// });
+
+// /* carrito De un producto */
+
+// let carrito = new Carrito(1);
+// let botones = document.querySelectorAll(".botonDeCompra");
+// let arrayDeBotones = Array.from(botones);
+// arrayDeBotones.forEach((boton) => {
+//   boton.addEventListener("click", (e) => {
+//     let productoSeleccionado = Catalogo.find(
+//       (producto) => producto.id == e.target.id
+//     );
+
+//     carrito.productos.push(productoSeleccionado);
+//     limpiarCarrito();
+//     actualizarCarrito(carrito);
+//     renovarStorage();
+//   });
+// });
+
+//
+//
+//
+//
 // function inicio() {
 //   alert("Bienvenidos a nwwPC. Importamos Netbook al mejor precio!");
 //   let usuario = prompt("Ingresa tu nombre:");
